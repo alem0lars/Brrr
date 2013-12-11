e4c609a8a9c39acb7955be84c576a214135d0d98
Alessandro Molari <molari.alessandro@gmail.com>
2013-12-11 09:19:26 +0100

    Brenzo

--- Brenzo/app/models/endpoint.rb
+++ Brenzo/app/models/endpoint.rb
@@ -1,7 +1,7 @@
 class Endpoint
   include Mongoid::Document
   field :address, type: String
-  field :port, type: String
+  field :port, type: Integer
   has_many :can_be_in, class_name: "Service", inverse_of: :available_responders
   has_many :used_as_responder_in, class_name: "Netflow", inverse_of: :responder
   has_many :used_as_originator_in, class_name: "Netflow", inverse_of: :originator
--- Brenzo/config/initializers/secret_token.rb
+++ Brenzo/config/initializers/secret_token.rb
@@ -16,4 +16,4 @@
 # Using secret_token for rails3 compatibility. Change to secret_key_base
 # to avoid deprecation warning.
 # Can be safely removed in a rails3 api-only application.
-Brenzo::Application.config.secret_token = 'ea120a3494435af82e35f81388f4a847b1cba95caf3332e495dd0b0b7e2b988cede59ee5dfcaee90498cff98df05a25820def3515a7ede47fd5f414e8b3290b9'
+Brenzo::Application.config.secret_token = '91f7467470c7725d257b1bf9032a0cdd4e8c8dff6f5a812586da5f86af66a6447b14806be1e544fe1d901ccd0bc78400dc6af409e28646da27d804ae78ee547e'
